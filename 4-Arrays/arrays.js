0c3cd599ef45912b08feb62218f8f976325decfb
David Gunner <gunnerjnr@live.co.uk>
2020-08-23 20:56:13 +0100

    4.3 for loops Exercise

--- 4-Arrays/arrays.js
+++ 4-Arrays/arrays.js
@@ -31,3 +31,70 @@ people.splice( 8, 2 )
 console.log( people )
 people.splice( 8, 2 )
 console.log( people )
+
+// 4.3 For Loops
+
+// Loop through the numbers and console.log each number within
+const numbers = [ 1, 12, 56, 84, 31, 9, 27, 2016, 2012, 29, 100 ]
+for ( i = 0; i < numbers.length; i++ )
+{
+    console.log( 'numbers[' + numbers[ i ] + ']' )
+}
+
+console.log( '--------------------------------------------------' )
+
+// Loop through the numbers. If the numbers are greater than 5, console.log them
+const numbersGreaterThanFive = [ 1, 12, 56, 4, 3, 9, 27, 26, 2, 29, 100 ]
+for ( i = 0; i < numbers.length; i++ )
+{
+    if ( numbersGreaterThanFive[ i ] > 5 )
+    {
+        console.log( 'numbers[' + numbersGreaterThanFive[ i ] + ']' )
+    }
+}
+
+console.log( '--------------------------------------------------' )
+
+// Create a new array. Add all numbers that are greater than 10 into this new array. (Hint: You have to loop through the numbers array first)
+const numbersArray = [ 1, 12, 4, 18, 9, 7, 11, 3, 50, 5, 6 ]
+// new array
+const newNumbersArray = []
+for ( num of numbersArray )
+{
+    // if the number in the array is greater than 10
+    if ( num > 10 )
+    {
+        // add it to the new array
+        newNumbersArray.push( num )
+    }
+}
+
+for ( num of numbersArray )
+{
+    console.log( 'numbers[' + num + ']' )
+}
+
+console.log( '--------------------------------------------------' )
+
+for ( newNum of newNumbersArray )
+{
+    console.log( 'numbers[' + newNum + ']' )
+}
+
+console.log( '--------------------------------------------------' )
+
+// Create a new array. Multiply all numbers by 5 and put them into the new array. (Hint: You have to loop through the numbers array first).
+const numbersArray2 = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+const anotherNewNumbersArray = []
+for ( num2 of numbersArray2 )
+{
+    // multiple the numbers by 5 and add to the new array
+    anotherNewNumbersArray.push( num2 * 5 )
+}
+
+for ( anotherNewNum of anotherNewNumbersArray )
+{
+    console.log( 'numbers[' + anotherNewNum + ']' )
+}
+
+console.log( '--------------------------------------------------' )
